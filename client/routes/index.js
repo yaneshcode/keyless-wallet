0b9bb9ada9d8df49153bbc62f8692752a67c4b70
yanesh <yaneshcode@gmail.com>
2019-04-25 22:52:45 -0400

    routes

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var walletController = require('../controllers/walletController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/', walletController.index);
+
+router.get('/getBalance/:addr', walletController.getBalance);
+
+router.post('/generateAddress', walletController.generateAddress);
+
+router.post('/deployWallet', walletController.deployWallet);
+
+router.post('/changeWalletOwner', walletController.changeWalletOwner);
+
 
 module.exports = router;
